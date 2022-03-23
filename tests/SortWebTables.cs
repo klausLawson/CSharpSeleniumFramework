b9ef7fdcd6a5f751591f3322d30a880e19e0d9e1
klauslawson <klauslawsondjecky56@gmail.com>
2022-03-23 14:29:32 -0500

    first commit

---
+++ tests/SortWebTables.cs
@@ -0,0 +1,58 @@
+﻿using CSharpSeleniumFramework.utilities;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumStudy
+{
+    class SortWebTables : Base
+    {
+        [Test]
+        public void SortTables()
+        {
+            ArrayList a = new ArrayList();
+            ArrayList b = new ArrayList();
+            SelectElement dropdown = new SelectElement(driver.FindElement(By.Id("page-menu")));
+            dropdown.SelectByValue("20");
+            //step 1 - Get all veggie into arraylist
+            IList<IWebElement> veggies = driver.FindElements(By.XPath("//td[1]"));
+            foreach (IWebElement veggie in veggies)
+            {
+                a.Add(veggie.Text);
+            }
+
+            //step 2 - sort this arrayList
+            foreach (String element in a)
+            {
+                TestContext.Progress.WriteLine(element);
+            }
+            TestContext.Progress.WriteLine("After Sorting");
+            a.Sort();
+            foreach (String element in a)
+            {
+                TestContext.Progress.WriteLine(element);
+            }
+
+            //step 3 - Go and Click column
+            //th[contains(@aria-label,'fruit name')]
+            driver.FindElement(By.CssSelector("th[aria-label *= 'fruit name']")).Click();
+
+            //step 4 Get all veggie names into arrayList B
+            IList<IWebElement> sortedVeggies = driver.FindElements(By.XPath("//td[1]"));
+            foreach (IWebElement veggie in sortedVeggies)
+            {
+                b.Add(veggie.Text);
+            }
+            //arraylist A to b equal
+            Assert.AreEqual(a, b);
+        }
+    }
+}
---
+++ utilities/Base.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace CSharpSeleniumFramework.utilities
+{
+    class Base
+    {
+        public IWebDriver driver;
+        [SetUp]
+        public void StartBrowser()
+        {
+            String browserName = ConfigurationManager.AppSettings["browser"];
+            InitBrowser(browserName);
+            // implicit wait 5sec can be declare globaly
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://rahulshettyacademy.com/loginpagePractise";
+        }
+        public void InitBrowser(String browserName)
+        {
+            switch (browserName)
+            {
+                case "Firefox":
+                    new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
+                    driver = new FirefoxDriver();
+                    break;
+                case "Chrome":
+                    new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+                    driver = new ChromeDriver();
+                    break;
+                case "Edge":
+                    new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
+                    driver = new EdgeDriver();
+                    break; 
+            }
+        }
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+        }
+    }
+}
